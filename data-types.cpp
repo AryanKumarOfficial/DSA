a35cc00a4c33b74fbd353c9e2353c3222365f98d
Aryan Kumar <Aryanak9163@gmail.com>
2024-07-06 00:33:14 +0530

    data types

---
+++ data-types.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+string name = "aryan"; // global variable
+int main()
+{
+    int a = 4, b = 6;
+    float pi = 1.014141;
+    char choice = 'b';
+    bool isAdmin = true;
+    int number_of_books_in_a_self = 10.5;
+    cout << "data types" << endl;
+    cout << "the value of a is " << a << " and b is " << b << endl;
+    cout << "the value of pi is " << pi << endl;
+    cout << "user choice\x20" << choice << endl;
+    cout << name << endl;
+    cout << isAdmin << endl;
+    cout << number_of_books_in_a_self;
+    return 0;
+}
