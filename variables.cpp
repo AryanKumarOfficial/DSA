f54e397910477309ccfdc86df2b4fa72288d3d30
Aryan Kumar <Aryanak9163@gmail.com>
2024-07-05 23:48:32 +0530

    variables and comments

---
+++ variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // single line comments
+    /**
+     * this is multi-
+     * line
+     * comment
+     */
+    int sum = 6;
+
+    cout << "hello world" << sum;
+    return 0;
+}
