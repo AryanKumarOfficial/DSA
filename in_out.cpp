86324b5037dd549f484c3dec440938991e903a41
Aryan Kumar <Aryanak9163@gmail.com>
2024-07-06 20:38:15 +0530

    input and output

---
+++ in_out.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /**
+     * Insertion operator - '<<'
+     * Excertion operator - '>>'
+     */
+     int num1, num2;
+    cout << "Enter the value of num 1 " << endl;
+    cin >> num1;
+
+    cout << "Enter the value of num 2 " << endl;
+    cin >> num2;
+
+    cout << "Sum is " << num1 + num2;
+}
